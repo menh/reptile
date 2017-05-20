0c2486e2eee8d514f33815de983e334697132f37
menh <872204778@qq.com>
2017-05-20 09:51:05 +0800

    当当网爬虫初版

--- DangDangWang/BSDDW.py
+++ DangDangWang/BSDDW.py
@@ -8,6 +8,7 @@ import urllib2
 import urllib
 import zlib
 import sys
+import re
 from bs4 import BeautifulSoup
 type = sys.getfilesystemencoding()
 class DDWspider(object):
@@ -17,10 +18,26 @@ class DDWspider(object):
         request=urllib2.Request(url)
         response=urllib2.urlopen(request)
         soup=BeautifulSoup(response)
-        print soup.select('ul[class="bigimg"]').encoding(type)
+
+        EveryLine=[]
+        info={}
+        line=1
+        file=open('title2.txt','w+')
+        while soup.find_all('li',class_='line'+str(line)):
+            s=soup.find_all('li',class_='line'+str(line))
+            ss=s[0].encode('gb18030')
+            EveryLine.append(ss)
+            info['title']=re.search('.*?title="(.*?)".*?',ss,re.S).group(1)
+            info['price']=re.search('.*?<span class="search_now_price">(.*?)</span>.*?',ss,re.S).group(1)
+            print info['price']
+            print info['title']
+            file.write(info['title'])
+            file.write(info['price'])
+            file.write('\n')
+            line=line+1
         return soup
-if __name__ == '__main__':  
-    bookInfo=[]  
+if __name__ == '__main__':
+    bookInfo=[]
     url = 'http://category.dangdang.com/pg1-cp01.36.11.00.00.00-shlist.html'
     dangdangSpider=DDWspider();
     html=dangdangSpider.getSource(url)
